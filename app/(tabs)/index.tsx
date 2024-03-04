376505aa0a1aafe4f030068d4ed4e2e82d1ea3c6
lizlo <38144471+lzldev@users.noreply.github.com>
2024-03-04 20:06:44 +0000

    dev container

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from 'tamagui'
+import { Text, View,Input } from 'tamagui'
 
 export default function TabOneScreen() {
   return (
     <View flex={1} alignItems="center">
       <Text fontSize={20}>Tab One</Text>
+      <Input
+      
+      />
     </View>
   )
 }
+
